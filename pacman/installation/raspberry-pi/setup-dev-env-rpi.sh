c70c02e707d04f9547b901e0226ec0576dd8e8cd
james94 <jamesmedel94@gmail.com>
2021-03-14 16:10:01 -0500

    Update setup script to install pgzero. Still debugging issues with running it

--- pacman/installation/raspberry-pi/setup-dev-env-rpi.sh
+++ pacman/installation/raspberry-pi/setup-dev-env-rpi.sh
@@ -1,3 +1,10 @@
+##
+# Install Pygame Zero 1.2 on Raspberry Pi 4 in miniconda dev environment
+##
+
+# Uninstall pygame if you notice issues trying to run existing pgzero
+# sudo apt -y autoremove python3-pygame
+
 # Download miniconda installation (32-bit version)
 curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-armv7l.sh" -o "Miniconda.sh"
 
@@ -10,5 +17,18 @@ conda config --add channels rpi
 # Update conda
 conda update conda
 
-# Install Pygame Zero
-pip install pgzero
+# Create conda environment called pacman
+conda create -y -n pacman python=3.5
+
+# Activate pacman environment
+source activate pacman
+
+# Install Pygame 1.9.4
+sudo apt -y install libatlas-base-dev
+sudo apt -y install python3-pygame
+
+# Install Pygame 1.9.4
+pip install pygame==1.9.4
+
+# Install Pygame Zero. Dependency Pygame 1.9.4
+pip install pgzero==1.2
